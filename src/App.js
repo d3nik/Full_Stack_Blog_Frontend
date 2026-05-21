d1ff7decafccc0a9d06325108c85414df431bf61
d3nik <113850410+d3nik@users.noreply.github.com>
2026-05-21 18:33:46 +0300

    patch 2.5 "Profile implementation"
    – created and exported profile page;
    – added profile page route path;
    – added header profile link button;
    – some header style changes.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { fetchUserMe, isAuthSelector } from './redux/slices/auth';
 import React from 'react';
 
 import { Header } from './components';
-import { Home, FullPost, Registration, AddPost, Login } from './pages';
+import { Home, Profile, FullPost, Registration, AddPost, Login } from './pages';
 
 function App() {
   const dispatch = useDispatch();
@@ -22,6 +22,7 @@ function App() {
       <Container maxWidth="lg">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/posts/:id' element={<FullPost />} />
           <Route path='/posts/:id/edit' element={<AddPost />} />
           <Route path='/add-post' element={<AddPost />} />
